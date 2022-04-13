c9cfae95112b7d14f5453970c730896da5aa5c83
Douglas Ribeiro Silverio <douglas.silverio.ti@gmail.com>
2022-04-13 00:32:49 -0300

    First Commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+Y
+)app/src/main/res/layout/activity_main.xml,7\6\76b07e40f24cce093536731477540f12e7da3cb6
+m
+=app/src/main/java/br/com/douglasti/fretefacil/MainActivity.kt,6\8\68d7d15d79df70da1a64ba073e1e2594fe439071
---
+++ app/src/main/java/br/com/douglasti/fretefacil/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.douglasti.fretefacil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
